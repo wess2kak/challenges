7c4ea008714f8b36d69bd74439f4d4ba56ed0456
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-11 22:45:34 -0400

    reformatted

--- repetitions.py
+++ repetitions.py
@@ -1,21 +1,20 @@
-def repetitions(input):
- substring_end = 1
- while substring_end <= len(input) * 0.5:
-  substring = input[0:substring_end]
-  quotient = int(len(input) / len(substring))
-  matches = 0
-  for attempt in range(quotient):
-   start = len(substring) * attempt
-   segment = input[start:start+len(substring)]
-   if segment == substring:
-    matches += 1
-  if matches > 1 and matches == quotient:
-   return True
-  substring_end += 1
- return False
+def substring_repetitions(input_str):
+    substring_end = 1
+    while substring_end <= len(input_str) * 0.5:
+        substring = input_str[0:substring_end]
+        matches = 0
+        for attempt in range(int(len(input_str) / len(substring))):
+            segment = input_str[len(substring) * attempt:(len(substring) * attempt) + len(substring)]
+            matches += 1 if segment == substring else 0
+        if matches > 1 and matches == int(len(input_str) / len(substring)):
+            return True
+        substring_end += 1
+    return False
+
 
 if __name__ == '__main__':
- test_words = ['abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123']
- print('Are the following strings made up of repeating segments?')
- for word in test_words:
-  print(repetitions(word),'\t',word)
+    test_words = ['abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123']
+    print('Are the following strings made up of repeating segments?')
+    for word in test_words:
+        print(substring_repetitions(word), '\t', word)
+
