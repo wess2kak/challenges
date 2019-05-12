071519c4790dfec39cde32660456f0cd3439f8d6
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-11 22:36:26 -0400

    returns true if an input string contains only repetitions

---
+++ repetitions.py
@@ -0,0 +1,21 @@
+def repetitions(input):
+ substring_end = 1
+ while substring_end <= len(input) * 0.5:
+  substring = input[0:substring_end]
+  quotient = int(len(input) / len(substring))
+  matches = 0
+  for attempt in range(quotient):
+   start = len(substring) * attempt
+   segment = input[start:start+len(substring)]
+   if segment == substring:
+    matches += 1
+  if matches > 1 and matches == quotient:
+   return True
+  substring_end += 1
+ return False
+
+test_words = ['abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123']
+
+print('Are the following strings made up of repeating segments?')
+for word in test_words:
+ print(repetitions(word),'\t',word)
