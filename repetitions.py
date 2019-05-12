a90a9bcf09d5ef83dcace76d6c61b7fcb64bd3b8
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-11 23:08:41 -0400

    fix for some odd strings

--- repetitions.py
+++ repetitions.py
@@ -6,15 +6,15 @@ def substring_repetitions(input_str):
         for attempt in range(int(len(input_str) / len(substring))):
             segment = input_str[len(substring) * attempt:(len(substring) * attempt) + len(substring)]
             matches += 1 if segment == substring else 0
-        if matches > 1 and matches == int(len(input_str) / len(substring)):
+            last_letter_checked = segment[-1]
+        if matches > 1 and matches == int(len(input_str) / len(substring)) and last_letter_checked == input_str[-1]:
             return True
         substring_end += 1
     return False
 
 
 if __name__ == '__main__':
-    test_words = ['abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123', '123a345b']
+    test_words = ['asdfasdfa', 'abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123', '123a345b']
     print('Are the following strings made up of repeating segments?')
     for word in test_words:
         print(substring_repetitions(word), '\t', word)
-
