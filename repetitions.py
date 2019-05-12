d8e2965a28bd2fc84990b5d6a355de5aae53c2c5
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-11 23:01:27 -0400

    returns false if odd length

--- repetitions.py
+++ repetitions.py
@@ -1,5 +1,7 @@
 def substring_repetitions(input_str):
     substring_end = 1
+    if not len(input_str) % 2 == 0:
+        return False
     while substring_end <= len(input_str) * 0.5:
         substring = input_str[0:substring_end]
         matches = 0
@@ -13,7 +15,7 @@ def substring_repetitions(input_str):
 
 
 if __name__ == '__main__':
-    test_words = ['abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123']
+    test_words = ['abcabcabc', 'aaaaaaa', 'asdfasdf', 'aa', 'aaa', 'qwertyuiopwertyuiop', '123a123a', 'asdf123asdf123', '123a345b']
     print('Are the following strings made up of repeating segments?')
     for word in test_words:
         print(substring_repetitions(word), '\t', word)
